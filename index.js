6f99dec4c51d8a747b578ac1a061d67763f9f4bf
Daniel_Thomsen <tho13020@byui.edu>
2018-11-24 14:43:35 -0700

    Added DB

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 const port = process.env.PORT || 5001;
 const dbConnectionString = process.env.DATABASE_URL || "Something";
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
+
+
 app.get("/person", getPerson);
 
 app.listen(port, function(){
