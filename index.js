1148818bc00864e258662efea11d776008223698
Daniel_Thomsen <tho13020@byui.edu>
2018-11-24 16:04:08 -0700

    fixed ejs errors

--- index.js
+++ index.js
@@ -39,27 +39,27 @@ var result = {id: 238, first: "John", last: "Smith", birthdate: "1950-02-05"};
 
 
 
-//
-//const { Pool } = require('pg');
-//const pool = new Pool({
-//  connectionString: process.env.DATABASE_URL,
-//  ssl: true
-//});
-//
-//
-//app.get('/db', async (req, res) => {
-//    try {
-//      const client = await pool.connect()
-//      const result = await client.query('SELECT * FROM test_table');
-//      const results = { 'results': (result) ? result.rows : null};
-//      res.render('pages/db', results );
-//      client.release();
-//    } catch (err) {
-//      console.error(err);
-//      res.send("Error " + err);
-//    }
-//  })
-//
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 
 
 
